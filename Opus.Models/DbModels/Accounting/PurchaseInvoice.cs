8870e773b5975c419f4a35457f30d6e76a6a2420
Buğra Kaya <bugrakaya16@hotmail.com>
2022-06-20 18:10:49 +0300

    developing

---
+++ Opus.Models/DbModels/Accounting/PurchaseInvoice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opus.Models.DbModels.Accounting
+{
+    public class PurchaseInvoice
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime DocDate { get; set; }
+        public string DocNo { get; set; }
+        public int PaymentMethId { get; set; }
+        [ForeignKey("PaymentMethId")]
+        public PaymentMeth PaymentMeth { get; set; }
+        public DateTime PaymentTerm { get; set; }
+        [NotMapped]
+        //public IEnumerable<BuyingDetails> Enumerable_BuyingDetails { get; set; }
+        public float OutofVat { get; set; }
+        public float Vat { get; set; }
+        public float Discount { get; set; }
+        public float TotalAmount { get; set; }
+        //developing..
+    }
+}
